2c9cd05c95553a7b8ec6adce87b01fe119fc082e
grintzdel <mathis.oudin@etu.uca.fr>
2024-09-10 19:12:07 +0200

    📦 NEW:  added a Test slice

---
+++ src/slices/RichText/index.tsx
@@ -0,0 +1,24 @@
+import { Content } from "@prismicio/client";
+import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
+
+/**
+ * Props for `RichText`.
+ */
+export type RichTextProps = SliceComponentProps<Content.RichTextSlice>;
+
+/**
+ * Component for "RichText" Slices.
+ */
+const RichText = ({ slice }: RichTextProps): JSX.Element => {
+  return (
+    <section
+      data-slice-type={slice.slice_type}
+      data-slice-variation={slice.variation}
+    >
+      <PrismicRichText field={slice.primary.title} />
+      <PrismicRichText field={slice.primary.body} />
+    </section>
+  );
+};
+
+export default RichText;
