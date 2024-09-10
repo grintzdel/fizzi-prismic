f9d8ba99174ab078091a74faea1006b838c6d52a
grintzdel <mathis.oudin@etu.uca.fr>
2024-09-10 18:35:29 +0200

    📦 NEW: added font + layout for prismic

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,16 @@
-import type { Metadata } from "next";
+import localFont from "next/font/local";
+
+import { PrismicPreview } from "@prismicio/next";
+import { repositoryName } from "@/prismicio";
+
 import "./app.css";
+
+const alpino = localFont({
+  src: "../../public/fonts/Alpino-Variable.woff2",
+  display: "swap",
+  weight: "100 900",
+  variable: "--font-alpino",
+});
 
 export default function RootLayout({
   children,
@@ -7,10 +18,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="fr">
+    <html lang="fr" className={alpino.variable}>
       <body className="text-white">
         <main>{children}</main>
       </body>
+      <PrismicPreview repositoryName={repositoryName} />
     </html>
   );
 }
