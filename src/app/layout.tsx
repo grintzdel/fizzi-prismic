3ae9af90855e15573a4ca7395de3beec57a4dbde
grintzdel <mathis.oudin@etu.uca.fr>
2024-09-12 13:03:53 +0200

    📦 NEW: setup viewcanvas component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import { repositoryName } from "@/prismicio";
 import "./app.css";
 
 import Header from "@/components/Header";
+import ViewCanvas from "@/components/ViewCanvas";
 
 const alpino = localFont({
   src: "../../public/fonts/Alpino-Variable.woff2",
@@ -23,7 +24,10 @@ export default function RootLayout({
     <html lang="fr" className={alpino.variable}>
       <body className="overflow-x-hidden bg-yellow-300">
         <Header />
-        <main>{children}</main>
+        <main>
+          {children}
+          <ViewCanvas />
+        </main>
       </body>
       <PrismicPreview repositoryName={repositoryName} />
     </html>
