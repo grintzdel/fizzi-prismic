0e309eb6f4ab13de2f30cf0d6b0fd1996e545675
grintzdel <mathis.oudin@etu.uca.fr>
2024-09-18 14:54:15 +0200

    :white_check_mark: handled mobile

--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -14,7 +14,8 @@ import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
 import { View } from "@react-three/drei";
 import Scene from "./Scene";
 import { Bubbles } from "./Bubbles";
-import {useStore} from "@/hooks/useStore";
+import { useStore } from "@/hooks/useStore";
+import { useMediaQuery } from "@/hooks/useMediaQuery";
 
 gsap.registerPlugin(useGSAP, ScrollTrigger);
 
@@ -27,77 +28,80 @@ export type HeroProps = SliceComponentProps<Content.HeroSlice>;
  * Component for "Hero" Slices.
  */
 const Hero = ({ slice }: HeroProps): JSX.Element => {
-
   const ready = useStore((state) => state.ready);
+  const isDesktop = useMediaQuery("(min-width: 768px)", true);
 
-  useGSAP(() => {
-    if (!ready) return;
+  useGSAP(
+    () => {
+      if (!ready && isDesktop) return;
 
-    const introTl = gsap.timeline();
+      const introTl = gsap.timeline();
 
-    introTl
-      .set(".hero", { opacity: 1 })
-      .from(".hero-header .split-char", {
-        scale: 0.6,
-        y: 100,
-        opacity: 0,
-        delay: 0.3,
-        ease: "bounce",
-        stagger: 0.3,
-      })
-      .from(
-        ".hero-subheading",
-        {
+      introTl
+        .set(".hero", { opacity: 1 })
+        .from(".hero-header .split-char", {
+          scale: 0.6,
+          y: 100,
           opacity: 0,
-          y: 30,
+          delay: 0.3,
+          ease: "bounce",
+          stagger: 0.3,
+        })
+        .from(
+          ".hero-subheading",
+          {
+            opacity: 0,
+            y: 30,
+          },
+          "-=1",
+        )
+        .from(".hero-body", {
+          opacity: 0,
+          y: 10,
+        })
+        .from(".hero-button", {
+          opacity: 0,
+          y: 10,
+          duration: 0.6,
+        });
+
+      const scrollTl = gsap.timeline({
+        scrollTrigger: {
+          trigger: ".hero",
+          start: "top top",
+          end: "bottom bottom",
+          scrub: 1.5,
         },
-        "-=1",
-      )
-      .from(".hero-body", {
-        opacity: 0,
-        y: 10,
-      })
-      .from(".hero-button", {
-        opacity: 0,
-        y: 10,
-        duration: 0.6,
       });
 
-    const scrollTl = gsap.timeline({
-      scrollTrigger: {
-        trigger: ".hero",
-        start: "top top",
-        end: "bottom bottom",
-        scrub: 1.5,
-      },
-    });
-
-    scrollTl
-      .fromTo(
-        "body",
-        {
-          backgroundColor: "#FDE047",
-        },
-        {
-          backgroundColor: "#D9F99D",
-          overwrite: "auto",
-        },
-        1,
-      )
-      .from(".text-side-heading .split-char", {
-        scale: 1.3,
-        y: 40,
-        rotate: -25,
-        opacity: 0,
-        stagger: 0.1,
-        ease: "back.out(3)",
-        duration: 0.5,
-      })
-      .from(".text-side-body", {
-        y: 20,
-        opacity: 0,
-      });
-  }, {dependencies: [ready]});
+      scrollTl
+        .fromTo(
+          "body",
+          {
+            backgroundColor: "#FDE047",
+          },
+          {
+            backgroundColor: "#D9F99D",
+            overwrite: "auto",
+          },
+          1,
+        )
+        .from(".text-side-heading .split-char", {
+          scale: 1.3,
+          y: 40,
+          rotate: -25,
+          opacity: 0,
+          stagger: 0.1,
+          ease: "back.out(3)",
+          duration: 0.5,
+        })
+        .from(".text-side-body", {
+          y: 20,
+          opacity: 0,
+        });
+    },
+    { dependencies: [ready, isDesktop] },
+  );
 
   return (
     <Bounded
@@ -105,16 +109,18 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
       data-slice-variation={slice.variation}
       className="hero opacity-0"
     >
-      <View className="hero-scene pointer-events-none sticky top-0 z-50 -mt-[100vh] hidden h-screen w-screen md:block">
-        <Scene />
-        <Bubbles
-          count={300}
-          speed={5}
-          opacity={0.5}
-          bubbleSize={0.05}
-          repeat={true}
-        />
-      </View>
+      {isDesktop && (
+        <View className="hero-scene pointer-events-none sticky top-0 z-50 -mt-[100vh] hidden h-screen w-screen md:block">
+          <Scene />
+          <Bubbles
+            count={300}
+            speed={5}
+            opacity={0.5}
+            bubbleSize={0.05}
+            repeat={true}
+          />
+        </View>
+      )}
 
       <div className="grid">
         <div className="grid h-screen place-items-center">
